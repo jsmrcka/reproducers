db94490aaf11495d9f94ed15ab2158ee8e76b425
Josef Smrcka <jsmrcka@redhat.com>
2023-11-01 13:43:49 +0100

    Reproducer for config-yaml issue on Windows
    Related issues:
    https://github.com/quarkusio/quarkus/issues/36813

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+    @Inject
+    GreetingConfig greetingConfig;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return greetingConfig.message();
+    }
+}
